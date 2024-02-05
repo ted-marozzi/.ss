f03834809cb75a6aa880da9bed7b4b2590ecf23f
Ted Marozzi <edwardmarozzi@gmail.com>
2024-02-05 19:22:48 +1100

    Update sharedconfig.sh script

--- sharedconfig.sh
+++ sharedconfig.sh
@@ -2,7 +2,6 @@
 # Replace this files with your own config
 set -euo pipefail
 
-
 function _list() {
     echo "gc"
     echo "gcp"
@@ -69,22 +68,20 @@ function touchsh() {
     # Strip .sh if the user entered it
     filename=${filename%".sh"}
 
-    printf '#!/bin/bash\nset -euo pipefail\n' > "$filename.sh";
+    printf '#!/bin/bash\nset -euo pipefail\n' >"$filename.sh"
     chmod +x "./$filename.sh"
 }
-
-
 
 function kp() {
     npx kill-port "$1"
 }
 
-alias openzsh="code ~/.zshrc";
+alias openzsh="code ~/.zshrc"
 alias openhome="code ~"
 
 function replaceSymlink() {
-    sourcefile=$(readlink "$1");
-    rm "$1" && cp -a "$sourcefile" "$1";
+    sourcefile=$(readlink "$1")
+    rm "$1" && cp -a "$sourcefile" "$1"
 }
 
 function replaceAllSymlinks() {
@@ -94,15 +91,15 @@ function replaceAllSymlinks() {
 # start: vim config
 set -o vi
 
-function zle-keymap-select () {
+function zle-keymap-select() {
 
-case $KEYMAP in
+    case $KEYMAP in
 
-vicmd) echo -ne '\e[1 q';; # block
+    vicmd) echo -ne '\e[1 q' ;; # block
 
-viins|main) echo -ne '\e[5 q';; # beam
+    viins | main) echo -ne '\e[5 q' ;; # beam
 
-esac
+    esac
 
 }
 
@@ -110,9 +107,9 @@ zle -N zle-keymap-select
 
 zle-line-init() {
 
-zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
 
-echo -ne "\e[5 q"
+    echo -ne "\e[5 q"
 
 }
 
@@ -120,7 +117,7 @@ zle -N zle-line-init
 
 echo -ne '\e[5 q' # Use beam shape cursor on startup.
 
-preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+preexec() { echo -ne '\e[5 q'; } # Use beam shape cursor for each new prompt.
 
 # end: vim config
 
